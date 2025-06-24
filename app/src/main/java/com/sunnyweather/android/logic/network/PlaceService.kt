71badac242b2822efe628f7f5129606a70c553e5
zhusi94 <zhusi940531@gmail.com>
2025-06-24 11:02:50 +0800

    高德接口
    Change-Id: I3aa63b6448b9c3ceb8cb431372d58947176b2ba9

--- app/src/main/java/com/sunnyweather/android/logic/network/PlaceService.kt
+++ app/src/main/java/com/sunnyweather/android/logic/network/PlaceService.kt
@@ -4,10 +4,11 @@ import com.sunnyweather.android.SunnyWeatherApplication
 import com.sunnyweather.android.logic.model.PlaceResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface PlaceService {
-    @GET("v3/geocode/geo?output=JSON&key=${SunnyWeatherApplication.MapToken}")
+    @GET("v3/geocode/geo?output=JSON")
     fun searchPlaces(
         @Query("address") query: String,
         @Query("key") key: String
